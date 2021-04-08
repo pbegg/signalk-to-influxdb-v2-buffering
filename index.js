e7ece240058c75e531d0e512110c416f366ec53a
pbegg <>
2021-04-08 14:36:46 +1000

    connect to influxdb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Influxdb = require('influxdb-v2');
 const buffer = require('./buffer.js')
+const {InfluxDB, Point, HttpError} = require('@influxdata/influxdb-client')
 
 
 
@@ -20,40 +20,44 @@ module.exports = function (app) {
   let vesselfleet = app.getSelfPath('fleet')
 
 
-  let influxPost = function (options,influxdb,metrics) {
-    influxdb.write(
-      {
-        org: options.influxOrg, // [Required] your organization. You can set `orgID` if you prefer to use the ID
-        bucket: options.influxBucket, // [Required] your bucket
-        precision: 'ms' // precision of timestamp. Can be `ns` (nanoseconds), `us` (microseconds), `ms` (milliseconds) or `s` (seconds). The default is `ns`
-      },
-      metrics,
-    )
-    .then(resp => {
-      app.debug(resp + 'Successfully uploaded')
-      bufferResult = buffer.loadBuffer(options) 
-      if (bufferResult == false) {
-        return
-      }
-      else {      
-        app.debug('There are files in the buffer')
-        app.debug(JSON.stringify(bufferResult))
-        influxPost(options,influxdb,buffer.sendBuffer(bufferResult,options))
-      }
 
-    })
-    .catch(err => {
-      // Handle errors
-        buffer.buffer(bufferArray,options)
-        bufferArray= []
-        app.debug(`bufferring metrics because ${err.message}`);
-        const bufferResult = buffer.loadBuffer(options)
-        if (bufferResult != false) {
-          app.debug(`There are ${bufferResult.length} files in the buffer`)
-        }
-    })
-    
-  }  
+
+  //let influxPost = function (options,influxdb,metrics) {
+    //influxdb.write(
+      //{
+        //org: options.influxOrg, // [Required] your organization. You can set `orgID` if you prefer to use the ID
+        //bucket: options.influxBucket, // [Required] your bucket
+        //precision: 'ms' // precision of timestamp. Can be `ns` (nanoseconds), `us` (microseconds), `ms` (milliseconds) or `s` (seconds). The default is `ns`
+      //},
+      //metrics,
+    //)
+    //.then(resp => {
+      //app.debug(resp + 'Successfully uploaded')
+      //bufferResult = buffer.loadBuffer(options) 
+      //if (bufferResult == false) {
+        //return
+      //}
+      //else {      
+        //app.debug('There are files in the buffer')
+        //app.debug(JSON.stringify(bufferResult))
+        //influxPost(options,influxdb,buffer.sendBuffer(bufferResult,options))
+      //}
+//
+    //})
+    //.catch(err => {
+      //// Handle errors
+        //buffer.buffer(bufferArray,options)
+        //bufferArray= []
+        //app.debug(`bufferring metrics because ${err.message}`);
+        //const bufferResult = buffer.loadBuffer(options)
+        //if (bufferResult != false) {
+          //app.debug(`There are ${bufferResult.length} files in the buffer`)
+        //}
+    //})
+//    
+  //}  
+
+
   
 
   let modifyPath = function(path,values,signalkTimestamp,options) {
@@ -127,23 +131,38 @@ module.exports = function (app) {
 
   let _start = function(options) {
     app.debug(`${plugin.name} Started...`)
+    const url = options["influxHost"]
+    const token = options["influxToken"]
+    const org = options["influxOrg"]
+    const bucket = options["influxBucket"]
+    const writeOptions = options["writeOptions"]
+    app.debug(writeOptions)
 
-    const influxdb = new Influxdb({
-      host: options.influxHost,
-      token: options.influxToken 
-      })
+
+    const writeApi = new InfluxDB({url,token})
+   
 
 
-    influxUploadTimer = setInterval(function() {
-      app.debug (`Sending ${metricArray.length} metrics to be uploaded to influx`)
-      if (metricArray.length != 0) {
-        influxPost(options,influxdb,metricArray)
-        bufferArray = metricArray
-        metricArray = []
-      }
-      }
-      , options.uploadFrequency)
-    app.debug (`Interval Started, upload frequency: ${options.uploadFrequency}ms`)
+   	writeApi.getWriteApi(org,bucket,'ms',writeOptions)
+
+    //writeApi.useDefaultTags({vesselname: app.getSelfPath('name')})
+
+    //const influxdb = new Influxdb({
+      //host: options.influxHost,
+      //token: options.influxToken 
+      //})
+//
+//
+    //influxUploadTimer = setInterval(function() {
+      //app.debug (`Sending ${metricArray.length} metrics to be uploaded to influx`)
+      //if (metricArray.length != 0) {
+        //influxPost(options,influxdb,metricArray)
+        //bufferArray = metricArray
+        //metricArray = []
+      //}
+      //}
+      //, options.uploadFrequency)
+    //app.debug (`Interval Started, upload frequency: ${options.uploadFrequency}ms`)
 
     app.subscriptionmanager.subscribe(
       _localSubscription(options),
@@ -190,87 +209,137 @@ module.exports = function (app) {
     unsubscribes.forEach(f => f());
     unsubscribes = [];
 
-    if (influxUploadTimer) {
-        clearInterval(influxUploadTimer);
-    }
-    // clean up the state
-    influxUploadTimer = undefined;    
-  }
+    //if (influxUploadTimer) {
+        //clearInterval(influxUploadTimer);
+    //}
+    //// clean up the state
+    //influxUploadTimer = undefined;    
+  //}
+	}
 
 
-  const plugin = {
+ const plugin = {
+	   "id":"signalk-to-influxdb-v2-buffer",
+	   "name":"Signalk To Influxdbv2.0",
+	   "description":"Plugin that saves data to an influxdbv2 database - buffers data without internet connection",
+	   "schema":{
+	      "type":"object",
+	      "required":[
+	         "influxHost",
+	         "influxToken",
+	         "influxOrg",
+	         "influxBucket",
+	         "uploadFrequency"
+	      ],
+	      "properties":{
+	         "influxHost":{
+	            "type":"string",
+	            "title":"Influxdb2.0 Host URL"
+	         },
+	         "influxToken":{
+	            "type":"string",
+	            "title":"Influxdb2.0 Token"
+	         },
+	         "influxOrg":{
+	            "type":"string",
+	            "title":"Influxdb2.0 Organisation"
+	         },
+	         "influxBucket":{
+	            "type":"string",
+	            "title":"Influxdb2.0 Bucket"
+	         },
+	         "writeOptions":{
+	            "type":"object",
+	            "required":[
+	               "batchSize",
+	               "defaultTags",
+	               "flushInterval",
+	               "maxBufferLines",
+	               "maxRetries",
+	               "maxRetryDelay",
+	               "minRetryDelay",
+	               "retryJitter"
+	            ],
+	            "properties":{
+	               "batchSize":{
+	                  "type":"number",
+	                  "title":"Batch Size",
+	                  "default": 1000
+	               },
+	               "defaultTags":{
+	                  "type":"object",
+	                  "title":"Default Tags",
+	                  "default": "{vesselname: VesselNameHere}"
+	               },
+	               "flushInterval":{
+	                  "type":"number",
+	                  "title":"Flush Interval",
+	                  "default": 30000
+	               },
+	               "maxBufferLines":{
+	                  "type":"number",
+	                  "title":"Maximum Buffer Lines",
+	                  "default": 32000
+	               },
+	               "maxRetries":{
+	                  "type":"number",
+	                  "title":"Maximum Retries",
+	                  "default": 3
+	               },
+	               "maxRetryDelay":{
+	                  "type":"number",
+	                  "title":"Maximum Retry Delay",
+	                  "default": 5000
+	               },
+	               "minRetryDelay":{
+	                  "type":"number",
+	                  "title":"Minimum Retry Delay",
+	                  "default": 180000
+	               },
+	               "retryJitter":{
+	                  "type":"number",
+	                  "title":"Retry Jitter",
+	                  "default": 200
+	               }
+	            }
+	         },
+	         "pathArray":{
+	            "type":"array",
+	            "title":"Paths",
+	            "default":[
+	               
+	            ],
+	            "items":{
+	               "type":"object",
+	               "required":[
+	                  "path",
+	                  "interval"
+	               ],
+	               "properties":{
+	                  "path":{
+	                     "type":"string",
+	                     "title":"Signal K path to record"
+	                  },
+	                  "interval":{
+	                     "type":"number",
+	                     "title":"Record Interval",
+	                     "default":1000
+	                  }
+	               }
+	            },
+	            "uploadFrequency":{
+	               "type":"number",
+	               "title":"Frequency of batched write to Influxdb2.0 in ms",
+	               "default":30000
+	            }
+	         }
+	      }
 
-    id: 'signalk-to-influxdb-v2-buffer',
-    name: 'Signalk To Influxdbv2.0',
-    description: 'Plugin that saves data to an influxdbv2 database - buffers data without internet connection',
-
-    schema: {
-      type: 'object',
-      required: [
-        'influxHost',
-        'influxToken', 
-        'influxOrg',
-        'influxBucket',
-        'bufferDirectory',
-        'pathsFile',
-        'uploadFrequency'
-      ],
-      properties: {
-        influxHost: {
-          type: 'string',
-          title: 'Influxdb2.0 Host URL'
-        },
-        influxToken: {
-          type: 'string',
-          title: 'Influxdb2.0 Token'
-        },
-        influxOrg: {
-          type: 'string',
-          title: 'Influxdb2.0 Organisation'
-        },
-        influxBucket: {
-          type: 'string',
-          title: 'Influxdb2.0 Bucket'
-        },
-        bufferDirectory: {
-          type: 'string',
-          title: 'full path to directory where the buffer should be stored (note no at end of dir)',
-          default: '/home/pi/signalkbuffer'
-        },
-        pathArray: {
-          type: 'array',
-          title: "Paths",
-          default: [],
-          items: {
-            type: 'object',
-            required: ['path', 'interval'],
-            properties: {
-              path: {
-                type: 'string',
-                title: 'Signal K path to record'
-              },
-              interval: {
-                type: 'number',
-                title: 'Record Interval',
-                default: 1000
-              }
-            }
-        }
-        },
-        "uploadFrequency": {
-          type: 'number',
-          title: 'Frequency of batched write to Influxdb2.0 in ms',
-          default: 30000
-        }
-      }
-    },
-
-    start: _start,
-    stop: _stop
-   
-
-  };
-
-  return plugin;
+	   },
+	   	start: _start,
+	    stop: _stop
+	}
+  
+return plugin
 
 }
