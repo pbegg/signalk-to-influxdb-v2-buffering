4d826c6574e1ecf989a3f65dc3de787cb9bec6d9
plainolddave <2941290+plainolddave@users.noreply.github.com>
2022-08-27 13:56:52 +1000

    Revert "Update index.js"
    This reverts commit 45fb3fca23fe7b83407f3766b9ede911367d9812.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // Description: The plugin is designed to do batch writes to a cloud hosted influxdb2.0
 //              data base.The Plugin now uses the https://github.com/influxdata/influxdb-client-js
 //              library. If the conenction to the influxdb is down the batch of metrics should be
-//              buffered and re uploaded when the internet connection is re-established
+//              buffered and re-uploaded when the internet connection is re-established
 //
 // Repository:  https://github.com/pbegg/signalk-to-influxdb-v2-buffering
 //
@@ -15,10 +15,10 @@
 //              -   added ability to push data from contexts outside of 'vessel.self'
 //              -   added ability to expand properties of any measurement (i.e. building on previous ability
 //                  to expand position and attitude, now any measurement with multiple properties can be expanded)
-//              -   added ablity to add tags against each individual path
+//              -   added ability to add tags against each individual path
 //              -   added the context & path to the name of each measurement 
 //              -   improved handling of wildcard '*' for context and path 
-//              -   lots of unecessary refactoring and tidy-up
+//              -   lots of unnecessary refactoring and tidy-up
 
 const { InfluxDB, Point, HttpError } = require('@influxdata/influxdb-client')
 
@@ -26,26 +26,8 @@ module.exports = function (app) {
 
     let options;
     let writeApi;
-    let unsubscribes = [];
-    let tagAsSelf;
-    let selfContext;
+    let unsubscribes = []
 
-    let getSelfContext = function () {
-
-        // get the current 'vessel.self' context - this seems unnecessarily difficult due to 
-        // limitations in the signalK network and may cause inconsistant results depending on 
-        // whether UUID or MMSI is defined in the Vessel Base Data on the Server -> Settings page
-        const selfUuid = app.getSelfPath('uuid');
-        const selfMmsi = app.getSelfPath('mmsi');
-
-        if (selfUuid != null) { // not null or undefined value
-            return "vessels." + selfUuid;
-        } else if (selfMmsi != null) {
-            return "vessels.urn:mrn:imo:mmsi:" + selfMmsi.toString();
-        }
-        return null;
-    };
-    
     let getInfluxPoint = function (context, path, value, timestamp, pathTags) {
 
         // The Point object defines the value for a single measurement,
@@ -53,6 +35,13 @@ module.exports = function (app) {
         // Note:
         // - the methods .intField and.uintField aren't used as all numeric values are mapped to floatField
         // - any errors with floatField, stringField etc throw an exception thats caught by the calling function
+
+        // Backwards compatibility - don't include context if its not defined (i.e. and the default vessel.self is being used)
+        let measurement = path;
+        if (context) { 
+            let measurement = context + "." + path;
+        }
+
         const point = new Point(path)
             .timestamp(Date.parse(timestamp));
 
@@ -62,11 +51,7 @@ module.exports = function (app) {
                 break;
 
             case 'number':
-                // skip infinite values like (Fuel Economy) Speed/Rate, when fuel rate is 0
-                // https://github.com/pbegg/signalk-to-influxdb-v2-buffering/pull/13#discussion_r656656037
-                if (isFinite(value)) {
-                    point.floatField('value', value)
-                }
+                point.floatField('value', value)
                 break;
 
             case 'boolean':
@@ -79,30 +64,16 @@ module.exports = function (app) {
                 break;
         }
 
-        // Add path tags if any have been defined
-        if (pathTags != null) {
+        // Add tags if any have been defined
+        if (pathTags !== undefined) {
             pathTags.forEach(tag => {
                 point.tag(tag["name"], tag["value"]);
             });
         }
 
-        // Add a tag with the context
-        if (context != null && context.length > 0) {
-            point.tag("context", context);
-        }
-
-        // Add a tag {self: true} when the measurement originates from this vessel -
-        // this is reliant on an MMSI or UUID to be set in the Vessel Base Data on 
-        // the Server -> Settings page. Potentially it may be inconsistant depending 
-        // on what UUID / MMSI is set so can be turned off on the plugin settings page, 
-        // and manually added as a tag for individual path(s) if needed
-        if (tagAsSelf === true && context.localeCompare(selfContext) === 0) {
-            point.tag("self", true);
-        }
-
         app.debug(`Sending to InfluxDB-V2: '${JSON.stringify(point)}'`);
         return point
-    };
+    }
 
     let handleUpdates = function (delta, pathOption) {
 
@@ -119,26 +90,17 @@ module.exports = function (app) {
                 // iterate through each value received in the update
                 update.values.forEach(val => {
 
-                    // if the value is an object, it may have properties to be unpacked into separate measurements
-                    // note this is not recursive - it only unpacks direct properties at the first layer of the measurement
+                    // if the value is an object, it may have properties to be unpacked into seperate measurements
                     if (typeof val.value === 'object' && pathOption.expand === true) {
-                        for (const property in val.value) {
-                            writeApi.writePoint(getInfluxPoint(
-                                delta.context,
-                                (val.path + "." + property),
-                                val.value[property],
-                                update.timestamp,
-                                pathOption.pathTags));
+                        for (const prop in val.value) {
+                            const prop_path = val.path + "." + prop;
+                            const prop_val = val.value[prop]
+                            writeApi.writePoint(getInfluxPoint(delta.context, prop_path, prop_val, update.timestamp, pathOption.pathTags));
                         }
                     }
                     // otherwise just write a point with a single value to InfluxDB
                     else {
-                        writeApi.writePoint(getInfluxPoint(
-                            delta.context,
-                            val.path,
-                            val.value,
-                            update.timestamp,
-                            pathOption.pathTags));
+                        writeApi.writePoint(getInfluxPoint(delta.context, val.path, val.value, update.timestamp, pathOption.pathTags));
                     }
                 });
 
@@ -148,7 +110,7 @@ module.exports = function (app) {
                 app.error(`Error: skipping update for path '${path}' because value '${valuesString}' is invalid`)
             }
         });
-    };
+    }
 
     let _start = function (opts) {
 
@@ -156,13 +118,11 @@ module.exports = function (app) {
 
         // set variables from plugin options
         options = opts;
-        selfContext = getSelfContext();
-        const url = options["influxHost"];
-        const token = options["influxToken"];
-        const org = options["influxOrg"];
-        const bucket = options["influxBucket"];
-        const writeOptions = options["writeOptions"];
-        tagAsSelf = writeOptions["tagAsSelf"];
+        const url = options["influxHost"]
+        const token = options["influxToken"]
+        const org = options["influxOrg"]
+        const bucket = options["influxBucket"]
+        const writeOptions = options["writeOptions"]
 
         // create InfluxDB api object
         writeApi = new InfluxDB({
@@ -175,7 +135,7 @@ module.exports = function (app) {
             writeOptions);
 
         // add default (global) tags, if any have been defined
-        if (options.defaultTags != null) {
+        if (options.defaultTags !== undefined) {
             let defaultTags = {}
             options.defaultTags.forEach(tag => {
                 defaultTags[tag["name"]] = tag["value"];
@@ -189,7 +149,7 @@ module.exports = function (app) {
         options.pathArray.forEach(pathOption => {
 
             // create a subsciption definition
-            localSubscription = {
+             localSubscription = {
                 "context": pathOption.context,
                 "subscribe": [{
                     "path": pathOption.path,
@@ -207,19 +167,19 @@ module.exports = function (app) {
                 },
                 delta => {
                     // add a handler for this update
-                    // app.debug(`Received update: ${JSON.stringify(delta)}`);
+                    //app.debug(`Received update: ${JSON.stringify(delta)}`);
                     handleUpdates(delta, pathOption);
                 }
             );
             app.debug(`Added subscription to: ${JSON.stringify(localSubscription)}`);
         });
-    };
+    }
 
     let _stop = function (options) {
         app.debug(`${plugin.name} Stopped...`)
         unsubscribes.forEach(f => f());
         unsubscribes = [];
-    };
+    }
 
     const plugin = {
         "id": "signalk-to-influxdb-v2-buffer",
@@ -309,19 +269,12 @@ module.exports = function (app) {
                             "title": "Retry Jitter",
                             "description": "a random value of up to retryJitter is added when scheduling next retry",
                             "default": 200
-                        },
-                        "tagAsSelf": {
-                            "type": "boolean",
-                            "title": "Tag vessel measurements as 'self' if applicable",
-                            "description": "tag measurements as {self: true} when from vessel.self - requires an MMSI or UUID to be set in the Vessel Base Data on the Server->Settings page",
-                            "default": false
                         }
                     }
                 },
                 "defaultTags": {
                     "type": "array",
                     "title": "Default Tags",
-                    "description": "default tags added to every measurement sent to InfluxDB",
                     "default": [],
                     "items": {
                         "type": "object",
@@ -356,8 +309,8 @@ module.exports = function (app) {
                             "context": {
                                 "type": "string",
                                 "title": "SignalK context",
-                                "description": "context to record e.g.'self' for own ship, or 'vessels.*' for all vessels, or '*' for everything",
-                                "default": "self"
+                                "description": "context to record e.g.'vessels.self' for own ship, or 'vessels.*' for all vessels, or '*' for everything",
+                                "default": "vessels.self"
                             },
                             "path": {
                                 "type": "string",
